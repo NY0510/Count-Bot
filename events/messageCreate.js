f48ac1ce059dae42d704f410c2bc847324d0eef5
NY64 <me@ny64.kr>
2023-03-02 22:34:01 +0900

    as

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const rootPath = path.join(__dirname, "..");
 const { EmbedBuilder } = require("discord.js");
-const { extractNumber } = require("kor-to-number");
 
 const configPath = path.join(rootPath, "data", "channel.json");
 
@@ -38,7 +37,7 @@ module.exports = {
 				.replaceAll(/곱하기|times|掛け算|かける/gi, "*")
 				.replaceAll(/나누기|divide|割り算|わる/gi, "/")
 				.replaceAll(/제곱|pow|二乗|にじょう/gi, "**");
-			calculateMsg = extractNumber(calculateMsg)[0].replace(/[^-()\d/*+^.]/g, "");
+			calculateMsg = calculateMsg.replace(/[^-()\d/*+^.]/g, "");
 			const evaledMessage = eval(calculateMsg);
 			const calculateNumber = Number(isRound ? Math.round(evaledMessage) : evaledMessage);
 
